c4a81e6180c8f7a2f5c1b326ee4a1b3107db93eb
sem-k32 <kirill.semkin32@mail.ru>
2025-04-10 13:12:08 +0300

    Modulated pendulum experiment

---
+++ experiments/pendulum/data.py
@@ -0,0 +1,34 @@
+""" Data generation utilities
+"""
+
+from typing import Callable
+
+import torch
+from torchdiffeq import odeint_adjoint as odeint
+
+from vf import PendulumVf
+
+
+def generate_trajectories(
+    g: Callable,
+    L: float,
+    z0: torch.FloatTensor,
+    sigma: torch.FloatTensor,   # diagonal covariance for noise
+    t: torch.FloatTensor,
+    method="dopri5",
+    rtol=1e-3
+) -> torch.FloatTensor:
+    vf = PendulumVf(g, L)
+    trajectories = odeint(
+         vf,
+         z0,
+         t,
+         rtol=rtol,
+         method=method
+    )
+    # place batch dimension first
+    trajectories = torch.swapaxes(trajectories, 0, 1)
+    # add noise
+    trajectories += sigma * torch.randn_like(trajectories, device=trajectories.device)
+
+    return trajectories
